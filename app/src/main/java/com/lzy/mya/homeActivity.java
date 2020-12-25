ce9858d90b2c56d141d17f2adcbd9d43002a73bd
emachies <2838632319@qq.com>
2020-12-25 19:15:48 +0800

    [update] 修改界面1.0

--- app/src/main/java/com/lzy/mya/homeActivity.java
+++ app/src/main/java/com/lzy/mya/homeActivity.java
@@ -1,14 +1,28 @@
 package com.lzy.mya;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class homeActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
